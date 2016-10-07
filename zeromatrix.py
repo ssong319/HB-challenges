9c204a01155f69710fe50aaf33959e264a995091
ssong <songstephanie03@gmail.com>
2016-10-07 11:22:23 -0700

    zero out a given matrix

---
+++ zeromatrix.py
@@ -0,0 +1,40 @@
+"""Given an NxM matrix, if a cell is zero, set entire row and column to zeroes.
+
+A matrix without zeroes doesn't change:
+
+    >>> zero_matrix([[1, 2 ,3], [4, 5, 6], [7, 8, 9]])
+    [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+But if there's a zero, zero both that row and column:
+
+    >>> zero_matrix([[1, 0, 3], [4, 5, 6], [7, 8, 9]])
+    [[0, 0, 0], [4, 0, 6], [7, 0, 9]]
+
+Make sure it works with non-square matrices:
+
+    >>> zero_matrix([[1, 0, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+    [[0, 0, 0, 0], [5, 0, 7, 8], [9, 0, 11, 12]]
+"""
+
+
+def zero_matrix(matrix):
+    """Given an NxM matrix, for cells=0, set their row and column to zeroes."""
+    lst = matrix
+    for i in range(len(lst)):
+        for k in range(len(lst[i])):
+            if lst[i][k] == 0:
+                target = k
+                for j in range(len(lst[i])):
+                    lst[i][j] = 0
+                for z in range(len(lst)):
+                    for x in range(len(lst[z])):
+                        if x == target:
+                            lst[z][x] = 0
+                return lst
+    return lst
+
+
+if __name__ == "__main__":
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** TESTS PASSED! YOU'RE DOING GREAT!\n"
